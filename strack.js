ac6bb6d55687c6d2a834a885cc51fdd2e2892c83
Adron Hall <adron_hall@homedepot.com>
2015-11-22 18:58:52 -0800

    Finished basic functionality for projects listing.

--- strack.js
+++ strack.js
@@ -1,69 +1,26 @@
 #!/usr/bin/env node
 
 var program = require('commander');
-var scmStash = require('./lib/hdqc/scmStash');
+var stashScm = require('./lib/hdqc/stashScm');
 
 var command = {};
 
 program
     .version('0.0.1')
-    .option('-P, --Projects', 'List Projects')
-    .option('-R, --Repositories', 'List All Repositories on Server.')
+    .option('-p, --projects', 'List Projects')
+    .option('-r, --repositories', 'List All Repositories on Server.')
     .arguments('<cmd>')
     .action(function (cmd) {
         command = cmd;
     })
+    .parse(process.argv);
 
-function listProjects() {
-    scmStash.getProjectListing(function (data) {
-        for (var i = 0; i < data.size; i++) {
-            var project = data.values[i];
-            console.log('    ' + i + ' ' + project.name + ' @ ' + project.link.url);
-        }
-    })
-}
+if (program.repositories && program.projects) {
+    stashScm.listProjects();
+} else if (program.projects) {
+    stashScm.listProjectsRepositories();
+} else if (program.repositories) {
 
-function getRepos(name, url, key) {
-    scmStash.getRepositories(function (repos) {
-        console.log('    ' + name + ' @ ' + url);
-        for (var r = 0; r < repos.size; r++) {
-            var repo = repos.values[r];
-            console.log('      ' + repo.name + ' ' + repo.cloneUrl);
-        }
-    }, key);
-}
-
-function getReposOnly(key) {
-    scmStash.getRepositories(function (repos) {
-        for (var r = 0; r < repos.size; r++) {
-            var repo = repos.values[r];
-            console.log('      ' + repo.name + ' ' + repo.cloneUrl);
-        }
-    }, key);
-}
-
-function listProjectsAndRepositories(reposOnly) {
-    scmStash.getProjectListing(function (data) {
-        for (var i = 0; i < data.size; i++) {
-            var project = data.values[i];
-            if (!reposOnly) {
-                getRepos(project.name, project.link.url, project.key);
-            } else {
-                getReposOnly(project.name, project.link.url, project.key);
-            }
-        }
-    })
-}
-
-if (program.Repositories && program.Projects) {
-    console.log('  - Projects and their repositories');
-    listProjectsAndRepositories(false);
-} else if (program.Projects) {
-    console.log('  - Projects');
-    listProjects();
-} else if (program.Repositories) {
-    console.log('  - Repositories');
-    listProjectsAndRepositories(true);
 }
 
 if (typeof command === 'undefined') {
@@ -73,5 +30,3 @@ if (typeof command === 'undefined') {
     console.log('Backing up started.');
     backup();
 }
-
-program.parse(process.argv);
---
+++ test/stashScmTests.js
@@ -0,0 +1,56 @@
+/**
+ * Created by Adron on 11/11/15.
+ * Description: tests (integration) for the scmStash library.
+ */
+
+var http = require('http');
+var assert = require('assert');
+var chai = require('chai').should();
+
+var stashScm = require('../lib/hdqc/stashScm');
+
+describe('stashScm', function () {
+
+    describe('when getting project information', function () {
+        it('should return project records', function () {
+
+        });
+
+        // it('should return project records', function (done) {
+        //     scmStash.getProjectListing(function (data) {
+        //         var itemToTest = data.values[0];
+        //         itemToTest.id.should.be.above(0);
+        //         done();
+        //     })
+        // });
+        //
+        // it('should return project records', function (done) {
+        //     scmStash.getProjectListing(function (data) {
+        //         var itemToTest = data.values[0];
+        //         itemToTest.key.should.not.be.empty;
+        //         done();
+        //     })
+        // });
+        //
+        // it('should return project records', function (done) {
+        //     scmStash.getProjectListing(function (data) {
+        //         var itemToTest = data.values[0];
+        //         itemToTest.link.url.should.not.be.empty;
+        //         done();
+        //     })
+        // });
+    });
+
+    // describe('when getting repositories information', function () {
+    //     it('should return repository records', function (done) {
+    //         scmStash.getProjectListing(function (data) {
+    //             var itemToTest = data.values[0];
+    //             scmStash.getRepositories(function (data) {
+    //                 var records = data.size;
+    //                 records.should.be.above(-1);
+    //                 done();
+    //             }, itemToTest.key);
+    //         })
+    //     })
+    // })
+});
