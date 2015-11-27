9f1365c0d73e1ee44b49a73e36fd698a754572e6
Adron Hall <adron_hall@homedepot.com>
2015-11-27 15:09:24 -0800

    Working on the test to resolve the request issue on the build.

--- test/stashScmTests.js
+++ test/stashScmTests.js
@@ -11,46 +11,47 @@ var stashScm = require('../lib/sources/stashScm');
 
 describe('stashScm', function () {
 
-    describe('when getting project information', function () {
-        it('should return project records', function () {
+  describe('when getting project information', function () {
 
-        });
+    it('junk test', function () {
+      true.should.be.equal(true);
+    })
 
-        // it('should return project records', function (done) {
-        //     scmStash.getProjectListing(function (data) {
-        //         var itemToTest = data.values[0];
-        //         itemToTest.id.should.be.above(0);
-        //         done();
-        //     })
-        // });
-        //
-        // it('should return project records', function (done) {
-        //     scmStash.getProjectListing(function (data) {
-        //         var itemToTest = data.values[0];
-        //         itemToTest.key.should.not.be.empty;
-        //         done();
-        //     })
-        // });
-        //
-        // it('should return project records', function (done) {
-        //     scmStash.getProjectListing(function (data) {
-        //         var itemToTest = data.values[0];
-        //         itemToTest.link.url.should.not.be.empty;
-        //         done();
-        //     })
-        // });
-    });
-
-    // describe('when getting repositories information', function () {
-    //     it('should return repository records', function (done) {
-    //         scmStash.getProjectListing(function (data) {
-    //             var itemToTest = data.values[0];
-    //             scmStash.getRepositories(function (data) {
-    //                 var records = data.size;
-    //                 records.should.be.above(-1);
-    //                 done();
-    //             }, itemToTest.key);
-    //         })
+    // it('should return project records', function (done) {
+    //     scmStash.getProjectListing(function (data) {
+    //         var itemToTest = data.values[0];
+    //         itemToTest.id.should.be.above(0);
+    //         done();
     //     })
-    // })
+    // });
+    //
+    // it('should return project records', function (done) {
+    //     scmStash.getProjectListing(function (data) {
+    //         var itemToTest = data.values[0];
+    //         itemToTest.key.should.not.be.empty;
+    //         done();
+    //     })
+    // });
+    //
+    // it('should return project records', function (done) {
+    //     scmStash.getProjectListing(function (data) {
+    //         var itemToTest = data.values[0];
+    //         itemToTest.link.url.should.not.be.empty;
+    //         done();
+    //     })
+    // });
+  });
+
+  // describe('when getting repositories information', function () {
+  //     it('should return repository records', function (done) {
+  //         scmStash.getProjectListing(function (data) {
+  //             var itemToTest = data.values[0];
+  //             scmStash.getRepositories(function (data) {
+  //                 var records = data.size;
+  //                 records.should.be.above(-1);
+  //                 done();
+  //             }, itemToTest.key);
+  //         })
+  //     })
+  // })
 });
